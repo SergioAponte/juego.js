92362c8b1aae3c93819b75e2475c1d396e873fee
sergio <>
2023-02-27 13:40:07 -0500

    juego

---
+++ main.js
@@ -0,0 +1,10 @@
+let ganadas=0
+let perdidas=0
+let empates=0
+let piedra=document.getElementById("piedra")
+let papel=document.getElementById("papel")
+let tijera=document.getElementById("tijera")
+document.getElementById('btn_jugar')
+function jugar(){
+
+}
